2bb3cc7e737ff19200ab361b5237efe485edd032
Iolanda Hilary Duarte Marques Batalha <111776556+Iolanda-Hilary@users.noreply.github.com>
2023-05-18 20:33:16 -0300

    Atualizações
    Falta adicionar o mecanismo de perguntar se o usuário se ele já sabe a palavra inteira

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ acertou = acertadas(leitor)
 
 perdeu = False
 ganhou = False
-erros = 0
+errosu = 0
 
 
 print(acertou)
@@ -28,14 +28,19 @@ while not perdeu and not ganhou:
         letrasFaltando = str(acertou.count('_'))
         if (letrasFaltando == "0"):
             print("PARABÉNS!! Você encontrou todas as letras formando a palavra '{}'".format(leitor).upper())
-        else:
-                erros += 1
+    else:
+                errosu += 1
                 print(acertou)
-                print('Ainda faltam acertar {} letras'.format(letrasFaltando))
-                print('Você ainda tem {} tentativas'.format(10-erros))
-                forca(erros)
-
-    perdeu = erros == 10
+                forca(errosu)
+                print('Errou, ainda faltam acertar {} letras'.format(letrasFaltando))
+                print('Você ainda tem {} tentativas'.format(10-errosu))
+    
+    perdeu = errosu == 10
     ganhou = "_" not in acertou
     print(acertou)
-    tudo=print("você já sabe a palavra?")
+
+if (acertou):
+    mensagemVencedor()
+else:
+    mensagemPerdedor(leitor)
+print("Fim do jogo, a palavra era", leitor)
